160bebd0c17065c86d68584b4533dc57d7506de2
Johannes Wünsche <johannes.wuensche97@gmail.com>
2020-07-04 00:26:11 +0200

    Begone Bootstrapper

--- setup_scion.sh
+++ setup_scion.sh
@@ -14,10 +14,9 @@ wait_for_node() {
     do
         sleep 5
         state=$(curl -X GET https://api.grid5000.fr/3.0/sites/"$1"/jobs/"$2" | jq '.state')
-        echo "$state"
+        echo "Current state is $state"
         if [ "$state" == \"running\" ]
         then
-            echo "me breaky breaky now"
             break
         fi
         echo "Node was not ready, waiting more.."
@@ -32,28 +31,14 @@ get_node_name() {
     echo "${name//\"/}"
 }
 
-wait_for_environment() {
-    # 1 - machine address/name
-    while true
-    do
-        sleep 10
-        if ssh -o PasswordAuthentication=no -o BatchMode=yes root@"$1" "echo 'ready'"
-        then
-            break
-        fi
-        echo "Waiting for environment to be deployed"
-    done
-    return 0
-}
-
 setup_machine() {
     # 1 - machine address/name
-    ssh root@"$1" "
+    ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" root@"$1" "
     useradd scionlab
-usermod -aG sudo scionlab
-echo 'scionlab ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
-mkdir -p /home/scionlab/.ssh
-echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDN2DRwQzmsywlXfve9GSS0nxMkLZUADBxRelGODXjm++BN2y/09ADwHc9Z9ihFS1P1DgAo3H180UtM2I2mtxpkE0RVNLt9/6l8pP7x+VR9KOy08Tzp/hsf47zFHEZQoDUPV4refuBOX4eTG/gLnOxJjmYKloov8ArnGd3jnOgK/OIGtjIhg6a8200wqFYCiDoZzc68TMtToSs37jJgY2ERjL0AUk/eOpvcovNBpFnyykBbAEbj/Fh3C+xv/pg/AVOLadIwxdM/rwLfMuhQvJPCD4dq+8mJ33plw8F+CSa06ZfE+hnTr69T9i2/tYSOaTk/fYEGlCit/pWVDkoy1JO1N7BKX+ZH4idyMzIuE3jU0qIglNEHzvjAsSfUTOuyRIs3NAqX2IB20SEFIHmJK5/MVi31cTZkjfGF9OiGiitA3rIQBz5nhzEQWg/g3HI9Z/J144gEIyclskH6ugyT4iBNHk6TQS4KBAxdigGPqfykw9WwsfGIeeBaoH6lqtM6z93ICglBHACIjnUjyx/uXNU9x+6JakTb7PHN32+F4DuRv+IkTR2Q6XcqYBju4jjZkUHsf93leghjiBSFtXT2XF1QAjnUxkJi/vy59CwJXapCpUWIn2TxKSZCriuX4uyOxqfXc767QsfAmVIQT5osDtucmQScfBeuY9MPTsMsARI2oQ== scion-ansible-key
+    usermod -aG sudo scionlab
+    echo 'scionlab ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
+    mkdir -p /home/scionlab/.ssh
+    echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDN2DRwQzmsywlXfve9GSS0nxMkLZUADBxRelGODXjm++BN2y/09ADwHc9Z9ihFS1P1DgAo3H180UtM2I2mtxpkE0RVNLt9/6l8pP7x+VR9KOy08Tzp/hsf47zFHEZQoDUPV4refuBOX4eTG/gLnOxJjmYKloov8ArnGd3jnOgK/OIGtjIhg6a8200wqFYCiDoZzc68TMtToSs37jJgY2ERjL0AUk/eOpvcovNBpFnyykBbAEbj/Fh3C+xv/pg/AVOLadIwxdM/rwLfMuhQvJPCD4dq+8mJ33plw8F+CSa06ZfE+hnTr69T9i2/tYSOaTk/fYEGlCit/pWVDkoy1JO1N7BKX+ZH4idyMzIuE3jU0qIglNEHzvjAsSfUTOuyRIs3NAqX2IB20SEFIHmJK5/MVi31cTZkjfGF9OiGiitA3rIQBz5nhzEQWg/g3HI9Z/J144gEIyclskH6ugyT4iBNHk6TQS4KBAxdigGPqfykw9WwsfGIeeBaoH6lqtM6z93ICglBHACIjnUjyx/uXNU9x+6JakTb7PHN32+F4DuRv+IkTR2Q6XcqYBju4jjZkUHsf93leghjiBSFtXT2XF1QAjnUxkJi/vy59CwJXapCpUWIn2TxKSZCriuX4uyOxqfXc767QsfAmVIQT5osDtucmQScfBeuY9MPTsMsARI2oQ== scion-ansible-key
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDTS3Euq1SXQ0wQsMrAdGgNhAJ7qHg/cSrDfCbZAOAj4/xOyEM5OFXrshtYDwbPqGA7Rtf4eq5ZCFSWaLh2aXSAr++I0X1av8rRXrizloWA2NjtkBVIJ/cuGty/lbNGFCa6KRgBOpd7c4lMTe/2AEAXM6azynMJJJ1eS8o+PZWcyEbCF36aOyF9kPoJ4by7gfXD3JGAXg3GltSBUURKzCvq9wCgvA9ZYApn+6Me1GrVbL0HhIm2mZXBVEYyqDSOQ9gjCz51K1eNp3kAabgxI5p9UEbJxN0emGHJt7PPOquZe9bbx/F7HxzzkOpatczyo45faj5u/s1y3vUohyy6E2GRSe/1rQVRxV7t4ENBUApt5Nd56fg0k/DGcnaVKnu0Kex0wY6jI4MwMS7DdB2cWsBkCCGwod8ybP7jDHLWC1siXY5bVA1ubCVeTNMgzpyWAgm8rqWlxtM6nqLTCb0oqPET7Y+q1ii8e+C5Y0Agd60Dq1Qi3TrSci2PsRI3hp6fuCawWVhpk/8yzCcAKKmfuCM8gTp+L7Y9IE99KBT1cfLMVNbA3FGC7SAJVCPSddCdxscwPZwAOfUSEKkQcWHqRO5PN32OogKq3liraCdVMfXlLOEGqpUJFnUnahQXLTHS+oHuHPPCDn/FjLAe2nivt7n0u4iTPQBhYFZIRHMmNCwEZQ== wirzf@inf.ethz.ch
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC5RjfMFXQhu6rhiuq4wCgkb02YXrrTZtUW9NwQaqbveBlbDnzCdmXA9aPXq7E4yZttVFyQc+skiyZ/JlQGaDHUCWf02Jq0qIUxx458xPmzYG3MT4C/HPDw3DMMbr36xij9FuHt5litZ7874vjBYkbfPzh4WS/78+tt8gtS6HZG1I+jpdi2ucJIlha5Z52u6ItavYZiOWaASr8vC59kCd1OdtO3gowJPAz6/38WJ4TTzJrkoRSdaMZxoYh07xwg2Zn+ulKGZPoH33mwL2JtUPowFtBKe5YdYftbfT7n0jMBVMXYR6XsQ9VL/mpwTkSUe3W6hrsF+nZdk0hSt86QLvRRY7x07VkD25l3oyufiMW7ZdpWO87JI9OTgZluNFf1Vh6BucNrjiSxnKHnhHs4naOcLMBbfJLjDAaFe4bXZ3aHKw0wZZGpUApYKe/9nJqlP4lmZNlWoSZESXFndZiqQMixIoynNG7k4vuVp8zoXCmD6AduFRTv59p3g2tB0fHaFyuoIV3M4ffghdqayTJDRFn8KpeRfko/GR5Zo4/0GSLRTDiI5pScLb0EAbtoWJJFuJDt8wtbMUrPaN/i5QAhXOq9mvmAcnVigeWcIcrebA9EAv47y1ePrlyFh3l2eBr65MicrPjD+S9i4qxS7ryDsHJdd10WCuhX9rET/2QHaHsdlw== juan.garcia@inf.ethz.ch
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDsmeslOcAxeDlcjS6aCFTU0SJckSHNg/Y7AFQj0R1dtJ//SMGv0StYTTrV7fCeALjVhzji0BJeE6VdJs5BZFz3lLZhq/JoSGYX2DHfLTclvch73sH7gM/u9nr7uyTLPZNauPCV+7lXt7tLqxyBb/hcr/j4k44U4nUR/R2ChPL3l7HFv3Iaxa61vNVzL8MIqcCCxd8lqZ4gUfZuP5aSZTdVE6CJdKY6pPz0ch0deL2+ndQMr2O8dqZUEJHwA4WB9SSrWoezgxOHyLkENhOPOPMqFMKwn8VasWmtYywyj0NjFRPKJdhIrJy/DNjJXavOMlAIbwwTcbdJNbSpuDL4t0cz jkwon@ict-networks-192-168-001-003.fwd-v4.ethz.ch
@@ -68,9 +53,29 @@ ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDmAztsnrE8u3IiKiPZ/ys+8eDr33BNO1FWlYDfVaaz
 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILwpdjGYdUL498BpaIfARfrqoe8WBaaEO+b5vBef4xxV matzf@thumper
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAEAQDaGpk/mUd0aDPYvLWbKvg8aDNkWbDw+xbuAjkHheXUmkNRAQqV2IlsNHGAtZRPH+SJlW+mRSUavVexcFPqyKb8VF9RYcdSWn+tjUvIoY81rIfO0gKYzr4G2ACWEV+vey1VlP7if8k3boNZbCG1EklwEnpl7P5fZoAsDhWJguh/9xIDEhq1bh6H7GfQJagcnS+rUvtXbqwqiPVDYKO3BJaALVukOcLdpxcEDQ5q4Qv5fNUBeQBliIKS7FrWsZNdVhJPi8TYWFmgyRGXzVj2EYX/vIcX0ezj7f5QIQacJUnSRg6eozYyO/4ESgRKjyn+toTENjrMChy7XjUIsqMWwshUX68OnzSBOhEgbkqCroYDVtHg4/OmgsVd9sJWWCT40Z0lfWVPISG/pCNrS4srUTyTyfA2SDyv+3WAWL4VG+lh/duFte2qKxRVOPKs/eg7gRUQJAGlYZBhl2FOWZGn5r60wX3Hh4zp0qEOyq2ULO8uCd4E46RqXL61kVHUL/v/yVmcB8XPmpAJaFJyASIzYNsre1oI6Axvhk8eUwWeZHKCl5tZckHh1D+Y+3fx9bkgsa12c44U1ErkRrLInK1uLdHuRMAN6DQpNJPcDzp/dWDzpBiZmIaW47mcQjgcjj+OEVoASX2s7QV6dis8wfg6yGl35bz3Xgd9YWigOPWFcRbTqeZCi2N0Wg6Fn9eoV9qBSrs+81t7LGRCGw7UtU4YDbMlIu6SnAEKFbGg2ylOvO5LEAdaMuWq14uC8HWigp2kLlkOlzE0ZBES5N65gLouAs44CcV7hj52sN4MdY4T7nh35Zrjks1h8nCcs5kvB2fE0imjD5R1YNHa37xQrHYajQ4xlYCXDiZkFX7O2gRT9gaLJ4NV8jcOIKRap7XgppcSswEV9X4MzmpyYkOcAVGqm/Bfeig46UOnY+TKn1VQVqSx9iWzCpyEWdHELh2ku1mH7yEWFlNQCfg0Y65KN6DTmQUvPzUSsS7lHDzvNdnyJ/g9dSgI9Vo7cOdM+pYPsMjFuDasUGdt9nlxjFOiv4clcIzuhGqhtOMhTNrSAdAqIQIc9Tv1p+R5b5nUDfYs5hCCylT/WBVfMPPowjYYsIzAGYOW6ka+kYLCWFPWjdPaQdq1LQXU+YIVbVZBoIyv50PD/IoWBNbeOBbHwqN4OHcp9Pobv449qrGz1VYvEFWLSGQNxS7RpNZQsbJ0w5FZaL9nEbNIWLJfr+6D78nHu7DycY0JCwmcKK7bmEVzgUIUfBPcHgl9+OsZE2MLxPTe61Pj/zhYE4UPnKQMd5xcHv0pd7p4TT40Vm4jkabdhBcTGpwmZTbxRfAGyNfpUyg0gjAQDlY3rMeZIXA0/Sh6X2cR4z51
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAEAQCxIhVVb4r+7/OGpEkWCfSh8jSqjPQsb4h2Jorxw+tikaxPEk4tawjcVzn29aKqHZswMQ/Zhc0F7uHVAiv4JXWbVIqG3fhe1r1ZHWGmAbftF4aMDxUVMj0J4xyO/TPHJbEChqAhx6LZHxRjQi2wGMGWbYcaINEN6IExc5C+SBqs8DJSBeF0TTkGhRFkgAlp7kPSVlyGkys/uhGPzniTpjk3ROjHqlCnDPCqWqxXqFbWAgZYAt7MvxiED/IoVRZqeFuEWPni0rhB40JuIkqLumbrUDVnbIP7yBVCHpJWvW03we4dUGxzkcQAdAVXJY1Icn9k7+IK3d/fSIh4T70J3VsHXwuAMLG5tHiIqUnoxAjxy30Wj5HzvaQnP9TkP627HdUbD6EXyJrwwxzi6MdQR14IRQfauzCBraCArm22BKeaNh3bjAv4URCm7I2GlJnF0oofufjJ9AbGq/6xjnDhCJIdgaYWNjbPj2GZRQNK18xzxCPvpHZDg9k37Jpgp+gUG0bbA7AayFapMFE1v0OoqvwHPamDUavKkP49Kfog1yeW5NzWy9wrZyP+ZVYRuKqqK548SWIw1rYCv8OETJWTtmD6zgmFeCNXIqE+PYxd9lXpQHKSfBCvwUYhLjXL3zm8J3UGGXsZorUDjChTfXILSeMOoldahej7dGNrU9aqstixMKVO3zIYjCllF7yO/0FqmoHqpCX1dp9ExIqsdlQUoqBszR2diOyfJV1lijTUbGPI8qcQKCSjaE4FpCFrRekNvkZlmqvQGY1wFe+kpXKvkbxFWK2AdxAv0i3f/HaytjSbx9riaG6usp2G/xgVkJTHFkzf4qGzVN030O0kvvz5Zy8C8depsib7x8KqTFGEMTPtPALs0JhFV6jK/U7sT6KK8xqwhijrcX/F4xTXExIoGjwR2RYCwiNO8qxT6aoFAHk1mYBRizTIrTJRfni2YKvIU00MaVyVdOGUuRws1CZNQzJaaM4IMOxQY+JPKy1BqSz5fiZSc1eedo6hXrW7G2cFIEbVT39ospum9nlUEWF+g12yxhXiMK3NmxU47d3jpI55919o9fQeV52houqJBQfWxeZOnD89VluMK9T7oUjvoNLDTKpCQuovTlnw0ZEWGsjVDEffarf8DCL1RhhTXsy8k83pxHVPsKT5J7K4sb4BWPef18OtLq+qvssEAJa5wTALlYKTrpY8EswkTSwGp4sNMQ35ZcS7shQwKeL+XqkqsS1+s3+zt7fAcfacwzDI6sCCiJiKkIwkyLbpDXJYy1sdkQEt3JTPbdNrRcshFfgaR1kkYvFJydvPQK0l3/kNa2KGdfpmmEL1uBEV/1wnwQRpYGZpyfert08SdP9VTVuPwx+B jwuensch@byzantium' > /home/scionlab/.ssh/authorized_keys
-chown -R scionlab /home/scionlab
-chsh -s /bin/bash scionlab
+    chown -R scionlab /home/scionlab
+    chsh -s /bin/bash scionlab
 "
+}
+
+wait_for_environment() {
+    # 1 - machine address/name
+    while true
+    do
+        sleep 10
+        if ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o PasswordAuthentication=no -o BatchMode=yes root@"$1" 'if [[ $(uname -a) =~ "Ubuntu" ]]; then exit 0; else exit 1; fi'
+        then
+            echo "Connecting possible, checking..."
+            if setup_machine "$1"
+            then
+                break
+            else
+                echo "Machine not ready yet, bootstrapping..."
+            fi
+        fi
+        echo "Waiting for environment to be deployed"
+    done
+    return 0
 }
 
 reserve_job() {
@@ -85,7 +90,7 @@ reserve_job() {
   # the $OAR_NODEFILE has to be evaluated on the machine itself, so disregard the shellcheck warning here
   node_result=$(curl https://api.grid5000.fr/3.0/sites/"$1"/jobs \
                   -X POST -H 'Content-Type: application/json' \
-                  -d "{\"resources\":\"{cluster='$2'}/nodes=1,walltime=1\",\"command\":\"kadeploy3 -k -e ubuntu1804-x64-min -f \$OAR_NODEFILE; sleep infinity\", \"name\":\"machine_$3_$4\", \"types\":[\"deploy\"]}")
+                  -d "{\"resources\":\"{eth_count>=2}/nodes=1,walltime=1\",\"command\":\"kadeploy3 -k -e ubuntu1804-x64-min -f \$OAR_NODEFILE; sleep infinity\", \"name\":\"machine_$3_$4\", \"types\":[\"deploy\"]}")
 
   vlan_job_id=$(curl https://api.grid5000.fr/3.0/sites/"$1"/jobs \
                   -X POST -H 'Content-Type: application/json' \
@@ -95,21 +100,14 @@ reserve_job() {
   wait_for_node "$1" "$node_id"
   wait_for_node "$1" "$vlan_job_id"
   node_name=$(get_node_name "$1" "$node_id")
-  
+
   echo "Adding interface to $node_name"
   sleep 10
   add_req="{\"nodes\":[\"$(echo "$node_name" | cut -d '.' -f 1)-eth1.$1.grid5000.fr\"]}"
   curl -d "$add_req" -X POST https://api.grid5000.fr/stable/sites/nancy/vlans/14 > /dev/null
 
   echo "Setting up machine $node_name"
-  while true
-  do
-    wait_for_environment "$node_name"
-    if setup_machine "$node_name"
-    then
-      break
-    fi
-  done
+  wait_for_environment "$node_name"
 }
 
 # reserve_job nancy gros 1293 1337
